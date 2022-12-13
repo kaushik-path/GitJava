f7d5319e00e1ad0339ae0cc5702cba43910f3631
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2022-12-13 12:45:31 +0530

    2

--- 01 JAVA basics/PrimeNum.java
+++ 01 JAVA basics/PrimeNum.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class PrimeNum {
     public static void main(String[] args) {
         int num, c=2;
-        Scanner sc = new Scanner(System.in);
-        num = sc.nextInt();
+        System.out.println("Enter a number");
+        Scanner scan = new Scanner(System.in); {
+            num = scan.nextInt();
+        }
         while (c<num) {
             if(num%c==0) {
                 System.out.println("not Prime Num");
