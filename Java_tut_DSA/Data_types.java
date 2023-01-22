33a6f96f84dd2c6b7b4889643fdcb32053995961
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-01-22 12:07:01 +0530

    input and output method

---
+++ Java_tut_DSA/Data_types.java
@@ -0,0 +1,23 @@
+public class Data_types {
+    public static void main(String[] args) {
+        System.out.println("Data types");
+        System.out.println("Primitive Data types");
+        //
+        int a =45;
+        // how to write int like in millions or Billions
+        int mORb = 1_800_000_000;
+        float b= 54.3f;
+        char c= 'k';
+        long longIntegers = 5413531332135L;
+        double largeDecimalNum = 1216513.5165132165;
+        boolean check = true;
+        //
+        System.out.println("Integer " +a);
+        System.out.println("One point eight billion " + mORb);
+        System.out.println("Float " +b);
+        System.out.println("Character " +c);
+        System.out.println("Long Integer "+longIntegers);
+        System.out.println("Long Decimals Number "+largeDecimalNum);
+        System.out.println("Boolen "+check);
+    }
+}
--- Java_tut_DSA/StandradOutnIn.java
+++ Java_tut_DSA/StandradOutnIn.java
@@ -10,8 +10,14 @@ public class StandradOutnIn {
         print: print the output only
         println: print the output in next line
         */
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine());
+        Scanner input = new Scanner(System.in); //-- Input method through Keyboard
+        // System.out.println(input.nextLine());
+
+        int num = input.nextInt(); // input decleared 
+        System.out.println(num); // output the input varible
+        float makrs = input.nextFloat();
+        System.out.println(marks);// output the input varible
+
 
     }
 }
