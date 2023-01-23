525abf4bfc81c48dbe64e7aa72e897e3e8e3bede
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-01-23 10:38:22 +0530

    Type Casting

---
+++ Java_tut_DSA/TypeCasting_Conversion.java
@@ -0,0 +1,18 @@
+public class TypeCasting_Conversion {
+    public static void main(String[] args) {
+        // Type casting
+        int num = (int)(2365.36598f);
+        System.out.println("Type casting of float(2365.36598f) into Int : "+ num);
+
+        //automatic type promotion in Expression 
+        int num1 = 257;
+        byte num2 = (byte)(num1); //257 % 256 = 1
+        System.out.println(num2);
+
+        byte a = 45;
+        byte b = 63;
+        byte c = 125;
+        int d = a * b / c ; // actual output in FLOAT 22.68 but it casting into INT
+        System.out.println(d);
+    }
+}
