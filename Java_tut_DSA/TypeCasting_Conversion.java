c95c8e060a0ea93b61aeb2eee1b2f47fa0b9c454
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-01-24 09:11:29 +0530

    type conversion Char to int

--- Java_tut_DSA/TypeCasting_Conversion.java
+++ Java_tut_DSA/TypeCasting_Conversion.java
@@ -1,5 +1,6 @@
 public class TypeCasting_Conversion {
     public static void main(String[] args) {
+
         // Type casting
         int num = (int)(2365.36598f);
         System.out.println("Type casting of float(2365.36598f) into Int : "+ num);
@@ -13,6 +14,13 @@ public class TypeCasting_Conversion {
         byte b = 63;
         byte c = 125;
         int d = a * b / c ; // actual output in FLOAT 22.68 but it casting into INT
-        System.out.println(d);
+        System.out.println("a * b / c :" +d);
+
+        // Type conversion --
+        int NUM3 = 'A';
+        System.out.println("Type Conversion : " +NUM3);
+
+        // UNICODE VALUES
+        System.out.println("Hello in Hindi : नमस्ते ");
     }
 }
