1b0a04f0f9bfe66b8addecb4ba19db5cd3bde146
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-02-22 20:47:45 +0530

    Conditionals in java

---
+++ Java_tut_DSA/Aa_Conditional_and_Loops.java
@@ -0,0 +1,19 @@
+/**
+ * Aa_Conditional_and_Loops
+ */
+public class Aa_Conditional_and_Loops {
+    public static void main(String[] args) {
+    
+        /*  IF ELSE LADDAR
+            Syntax of if statements:
+            if (boolean expression True or False) {
+                // body
+            } else if {
+                // do this
+            }else{
+                //Do this
+            }
+        */
+
+    }
+}
